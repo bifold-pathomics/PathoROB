8a40933fabe711f507b71800a21d1778d18567af
hense96 <julius@hense-ge.de>
2025-08-14 15:01:38 +0200

    integrated metadata loading into feature data manager

--- pathorob/features/data_manager.py
+++ pathorob/features/data_manager.py
@@ -1,16 +1,34 @@
+import warnings
 from pathlib import Path
 
+import pandas as pd
 import numpy as np
+
+
+METADATA_COLUMNS = ["subset", "slide_id", "patch_id", "biological_class", "medical_center"]
 
 
 class FeatureDataManager:
 
-    def __init__(self, features_dir, chunk_key="medical_center"):
+    def __init__(self, features_dir, metadata_dir="data/metadata", chunk_key="medical_center"):
         self.features_dir = Path(features_dir)
+        self.metadata_dir = Path(metadata_dir)
         self.chunk_key = chunk_key
 
     def _compute_ids(self, metadata):
         return metadata[["slide_id", "patch_id"]].agg("-".join, axis=1)
+
+    def get_available_datasets(self):
+        return sorted([
+            dataset_dir.stem for dataset_dir in self.features_dir.iterdir()
+            if dataset_dir.is_dir()
+        ])
+
+    def get_available_metadata(self):
+        return sorted([
+            metadata_dir.stem for metadata_dir in self.metadata_dir.iterdir()
+            if metadata_dir.is_file()
+        ])
 
     def save_features(self, dataset, features, metadata):
         # Create dataset path
@@ -36,7 +54,7 @@ class FeatureDataManager:
         # Load dataset path
         dataset_dir = self.features_dir / dataset
         if not dataset_dir.is_dir():
-            raise ValueError(f"Cannot find features for dataset '{dataset}' at '{dataset_dir}'.")
+            raise ValueError(f"Cannot find features for dataset '{dataset}' at: '{dataset_dir}'.")
         # Recover identifiers of the features
         metadata = metadata.copy()
         metadata.insert(0, "id", self._compute_ids(metadata))
@@ -47,9 +65,19 @@ class FeatureDataManager:
             # Load chunk of features
             chunk_file = dataset_dir / f"{chunk_id.replace(' ', '_')}.npz"
             if not chunk_file.is_file():
-                raise ValueError(f"Cannot find features for chunk '{chunk_id}' at '{chunk_file}'.")
+                raise ValueError(f"Cannot find features for chunk '{chunk_id}' at: '{chunk_file}'.")
             features_chunk = dict(np.load(chunk_file))
             features_dict.update({id: features_chunk[id] for id in metadata_chunk["id"].unique()})
         # Sort features according to given metadata structure
         features = np.stack([features_dict[id] for id in metadata["id"]])
         return features
+
+    def load_metadata(self, name):
+        metadata_path = self.metadata_dir / f"{name}.csv"
+        if not metadata_path.is_file():
+            raise ValueError(f"Cannot find metadata '{name}' at: '{metadata_path}'.")
+        metadata = pd.read_csv(metadata_path, index_col=None, dtype=str)
+        missing_cols = [col for col in metadata.columns if col not in METADATA_COLUMNS]
+        if len(missing_cols) > 0:
+            warnings.warn(f"Missing columns in metadata '{name}': {missing_cols}")
+        return metadata
